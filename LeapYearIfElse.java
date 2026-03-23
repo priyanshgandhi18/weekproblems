c6397f28f8793d71398ce059a080a6c0173328ad
Priyansh Gandhi <priyanshgandhi800@gmail.com>
2026-03-23 09:52:40 +0530

    Added solutions

---
+++ LeapYearIfElse.java
@@ -0,0 +1,32 @@
+package weekproblems;
+import java.util.Scanner;
+
+public class LeapYearIfElse {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a year: ");
+        int year = sc.nextInt();
+
+        if (year < 1582) {
+            System.out.println("Not a valid Gregorian year");
+        } else {
+            if (year % 4 == 0) {
+                if (year % 100 == 0) {
+                    if (year % 400 == 0) {
+                        System.out.println(year + " is a Leap Year");
+                    } else {
+                        System.out.println(year + " is NOT a Leap Year");
+                    }
+                } else {
+                    System.out.println(year + " is a Leap Year");
+                }
+            } else {
+                System.out.println(year + " is NOT a Leap Year");
+            }
+        }
+
+        sc.close();
+    }
+}
